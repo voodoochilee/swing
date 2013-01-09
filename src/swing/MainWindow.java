af0904ef79941b8804d26503fc662f9d0f675769
John Harding <d02124955@mydit.ie>
2013-01-09 14:41:20 +0000

    buttons

--- src/swing/MainWindow.java
+++ src/swing/MainWindow.java
@@ -1,5 +1,8 @@
 package swing;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 
@@ -15,8 +18,20 @@ public class MainWindow extends JFrame {
 	private JMenuItem cutMenuItem = new JMenuItem("Cut");
 	private JMenuItem copyMenuItem = new JMenuItem("Copy");
 	
+	private JLabel nameLabel = new JLabel("Name");
+	private JTextField nameTextField = new JTextField(20);
+	private JCheckBox qualifiedCheckBox = new JCheckBox("Qualified");
+	private JCheckBox completedCheckBox = new JCheckBox("Completed");
+	
+	private JRadioButton primaryRadioButton = new JRadioButton("Primary");
+	private JRadioButton secondaryRadioButton = new JRadioButton("Secondary");
+	private JRadioButton thirdLevelRadioButton = new JRadioButton("Third Level");
+	private ButtonGroup buttonGroup = new ButtonGroup();
+	
+	
 	
 	public MainWindow() {
+		this.setLayout(new FlowLayout());
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setJMenuBar(menuBar);
 		menuBar.add(fileMenu);
@@ -27,9 +42,26 @@ public class MainWindow extends JFrame {
 		fileMenu.add(saveAsMenuItem);
 		editMenu.add(cutMenuItem);
 		editMenu.add(copyMenuItem);
+		this.add(nameLabel);
+		this.add(nameTextField);
+		this.add(qualifiedCheckBox);
+		this.add(completedCheckBox);
+		this.add(primaryRadioButton);
+		this.add(secondaryRadioButton);
+		this.add(thirdLevelRadioButton);
+		buttonGroup.add(primaryRadioButton);
+		buttonGroup.add(secondaryRadioButton);
+		buttonGroup.add(thirdLevelRadioButton);
 	}
 	
 	public static void main(String[] args) {
+		String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+		try {
+			UIManager.setLookAndFeel(lookAndFeel);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		MainWindow window = new MainWindow();
 		window.setSize(500, 300);
 		window.setLocationRelativeTo(null);
